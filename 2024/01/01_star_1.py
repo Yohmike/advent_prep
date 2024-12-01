638300ba10ae4ed8384546400f70d4f8ce8134b7
Mike <mihai.plesea@gmail.com>
2024-12-01 11:09:57 +0000

    2024 day 1

---
+++ 2024/01/01_star_1.py
@@ -0,0 +1,29 @@
+"""
+https://adventofcode.com/2024/day/1
+
+"""
+
+from typing import List
+
+
+def parse_input_and_solve(filename):
+    left = []
+    right = []
+    with open(filename) as file:
+        for line in file:
+            ids = line.split(" ")
+            left.append(int(ids[0]))
+            right.append(int(ids[-1]))
+    left.sort()
+    right.sort()
+    return sum([abs(l - r) for l, r in zip(left, right)])
+
+
+def main():
+    input_file_name = "../input"
+    print(parse_input_and_solve(input_file_name))
+    return -1
+
+
+if __name__ == "__main__":
+    main()
---
+++ 2024/01/01_star_2.py
@@ -0,0 +1,28 @@
+"""
+https://adventofcode.com/2024/day/1
+
+"""
+
+from typing import List
+from collections import defaultdict
+
+
+def parse_input_and_solve(filename):
+    left = []
+    right = defaultdict(int)
+    with open(filename) as file:
+        for line in file:
+            ids = line.split(" ")
+            left.append(int(ids[0]))
+            right[int(ids[-1])] += 1
+    return sum([l * right.get(l, 0) for l in left])
+
+
+def main():
+    input_file_name = "../input"
+    print(parse_input_and_solve(input_file_name))
+    return -1
+
+
+if __name__ == "__main__":
+    main()
