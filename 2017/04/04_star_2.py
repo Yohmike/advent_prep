490a831e3aa149b162b3b5db9be44875ecfb75e0
Mike <mihai.plesea@gmail.com>
2024-12-02 17:25:01 +0000

    2017 day 4

---
+++ 2017/04/04_star_2.py
@@ -0,0 +1,30 @@
+"""
+https://adventofcode.com/2017/day/4
+
+"""
+
+from collections import defaultdict
+
+
+def parse_input_and_solve(filename):
+
+    with open(filename) as file:
+        valid_count = 0
+        for line in file:
+            passphrase = line[:-1].split(" ")
+            passphrase_dict = defaultdict(int)
+            for word in passphrase:
+                passphrase_dict["".join(sorted(word))] += 1
+            if max(passphrase_dict.values()) <= 1:
+                valid_count += 1
+    return valid_count
+
+
+def main():
+    input_file_name = "../input"
+    print(parse_input_and_solve(input_file_name))
+    return -1
+
+
+if __name__ == "__main__":
+    main()
