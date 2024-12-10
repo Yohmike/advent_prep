cafb6bd4b69eee20e5a4fa0199657679d4c416e3
Mike <mihai.plesea@gmail.com>
2024-12-10 13:08:02 +0000

    2024 day10

---
+++ 2024/10/10_star_2.py
@@ -0,0 +1,67 @@
+"""
+https://adventofcode.com/2024/day/10
+
+"""
+
+
+def find_trailheads(map):
+    trailheads = []
+    for i in range(len(map)):
+        for j in range(len(map[0])):
+            if map[i][j] == 0:
+                trailheads.append((i, j))
+
+    return trailheads
+
+
+def find_next_trail_steps(map, position, hike_index):
+    next_trail_step = []
+    row, col = position
+    if 0 <= row - 1 and map[row - 1][col] == hike_index:
+        next_trail_step.append((row - 1, col))
+    if row + 1 < len(map) and map[row + 1][col] == hike_index:
+        next_trail_step.append((row + 1, col))
+    if 0 <= col - 1 and map[row][col - 1] == hike_index:
+        next_trail_step.append((row, col - 1))
+    if col + 1 < len(map[0]) and map[row][col + 1] == hike_index:
+        next_trail_step.append((row, col + 1))
+
+    return next_trail_step
+
+
+def find_trail_rating(map, position):
+    trail = [position]
+    for i in range(1, 10):
+        new_trails = []
+        for t in trail:
+            new_trail = find_next_trail_steps(map, t, i)
+            new_trails.extend(new_trail)
+        if new_trails == []:
+            return []
+        trail = new_trails
+    return trail
+
+
+def parse_input_and_solve(filename: str) -> int:
+    update_count = 0
+    map = []
+    with open(filename) as file:
+        for line in file:
+            map.append([int(x) for x in line.strip("\n")])
+    trailheads = find_trailheads(map)
+
+    for trailhead in trailheads:
+        trails = find_trail_rating(map, trailhead)
+        # print(trails)
+        update_count += len(trails)
+    return update_count
+
+
+def main():
+    input_file_name = "../input"
+    print(parse_input_and_solve(input_file_name))
+    return -1
+
+
+if __name__ == "__main__":
+    main()
